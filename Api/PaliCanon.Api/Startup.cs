ddb72fa789c42aec59c8d4effb90c87fb10a4d29
timbrownls20 <timbrownls26@hotmail.com>
2018-01-03 19:53:15 +1000

    enabling cross domain posts

--- Api/PaliCanon.Api/Startup.cs
+++ Api/PaliCanon.Api/Startup.cs
@@ -23,12 +23,19 @@ namespace PaliCanon.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder => builder
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials());   
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();

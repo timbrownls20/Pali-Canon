7cdb1827b7aed9eed0b0ae64a14dbe3c8820f3cc
timbrownls20 <timbrownls26@hotmail.com>
2018-01-04 17:42:38 +1000

    add bootstrap styling via cdn

--- Client/Angular/pali-canon/src/app/app.component.ts
+++ Client/Angular/pali-canon/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, NgModule  } from '@angular/core';
+//import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+//import { AlertModule } from '@ng-bootstrap/components/alert';
 
 @Component({
   selector: 'app-root',
    templateUrl: './app.component.html',
    //styleUrls: ['./app.component.css']
-   styleUrls: ['../../node_modules/bootstrap/dist/css/bootstrap.min.css', './app.component.css', ]
+   styleUrls: ['./app.component.css', ]
 })
+
+
+// @NgModule({
+//   declarations: [AppComponent],
+//   imports: [NgbModule.forRoot()],
+//   bootstrap: [AppComponent]
+// })
+
 export class AppComponent {
   title = 'Pali Canon';
 }
--- Client/Angular/pali-canon/src/app/quote/quote.component.ts
+++ Client/Angular/pali-canon/src/app/quote/quote.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 import {Quote } from '../quote'
 import { QuoteService } from '../quote.service';
+//import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+// @NgModule({
+//   declarations: [NgbModule],
+//   imports: [NgbModule]
+// })
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
+
+
 export class QuoteComponent implements OnInit {
 
   quote: Quote; 

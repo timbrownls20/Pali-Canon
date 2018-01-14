28ff94e9a1924c43644c87661db50c3959e306e2
timbrownls20 <timbrownls26@hotmail.com>
2018-01-14 14:13:34 +1000

    improve referencing of global styles

--- Client/Angular/pali-canon/src/app/settings/settings.component.ts
+++ Client/Angular/pali-canon/src/app/settings/settings.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css', '../app.component.css']
+  styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
 

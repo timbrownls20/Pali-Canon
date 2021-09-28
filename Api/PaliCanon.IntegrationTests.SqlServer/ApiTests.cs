66e0618dc7d25a4884e50586bedffb5d9b0c06ed
timbrownls20 <timbrownls26@hotmail.com>
2021-09-28 16:09:38 +1000

    simple integration test for sql server quote

---
+++ Api/PaliCanon.IntegrationTests.SqlServer/ApiTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace PaliCanon.IntegrationTests.SqlServer
+{
+
+    [TestClass]
+    public class ApiTests
+    {
+        public const string ApiRoot = "http://localhost:65006/api/";
+
+        [TestMethod]
+        public async Task Quote()
+        {
+            //.. arrange
+            HttpClient client = new HttpClient();
+            
+            //.. act
+            var response = await client.GetAsync($"{ApiRoot}quote");
+            string content = await response.Content.ReadAsStringAsync();
+            var jsonResponse = JObject.Parse(content);
+            var quote = jsonResponse["text"];
+
+            //..assert
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Assert.IsNotNull(quote);
+        }    
+    }
+}

9d0bd2ec854b37be96b109ca3920243a62349e8f
timbrownls20 <timbrownls26@hotmail.com>
2021-11-06 14:44:05 +1000

    correct header styling

--- Client/react.typescript.palicanon/src/config.tsx
+++ Client/react.typescript.palicanon/src/config.tsx
@@ -1,13 +1,15 @@
 interface IConfig {
     api:string,
     interval:number,
-    imageNumber:number | null
+    imageNumber:number | null,
+    imageCount:number
 }
 
 const config:IConfig = {
     api:'http://palicanon.codebuckets.com.au/api/quote',
     interval: 1500,
-    imageNumber:null
+    imageNumber:10,
+    imageCount:10
 }
 
 export default config;
